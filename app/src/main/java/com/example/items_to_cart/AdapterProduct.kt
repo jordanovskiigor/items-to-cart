5d5fcf4ef27fe0e7c7c8511c108866f089e9c8fe
jordanovskiigor <ijordanovski.mk@gmail.com>
2023-12-23 21:00:23 +0100

    Cart badge functionality implementation -Update badge visibility based on item count -Increment badge count when adding items to cart

--- app/src/main/java/com/example/items_to_cart/AdapterProduct.kt
+++ app/src/main/java/com/example/items_to_cart/AdapterProduct.kt
@@ -7,16 +7,42 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.LinearLayout
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class AdapterProduct(val products: List<Product>) :
     RecyclerView.Adapter<AdapterProduct.ProductViewHolder>() {
 
+    //interface for click event
+    interface OnAddToCartClickListener{
+        fun onAddToCartClick(position: Int)
+    }
+
+    //click listener variable
+    private var addToCartClickListener:OnAddToCartClickListener? = null
+
+    //Setter method for click listener
+    fun setOnAddToCartClickListener(listener:OnAddToCartClickListener) {
+        this.addToCartClickListener = listener
+    }
+
     inner class ProductViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView)  {
         var productTitle: TextView = itemView.findViewById(R.id.tv_product_title)
         var productPrice: TextView = itemView.findViewById(R.id.tv_product_price)
         var productImage: ImageView = itemView.findViewById(R.id.iv_product)
+        var addToCart: TextView = itemView.findViewById(R.id.ic_add_to_cart)
+
+
+        init {
+            addToCart.setOnClickListener {
+                val position = adapterPosition
+                if (position != RecyclerView.NO_POSITION) {
+                    addToCartClickListener?.onAddToCartClick(position)
+
+                }
+            }
+        }
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ProductViewHolder {
--- app/src/main/java/com/example/items_to_cart/MainActivity.kt
+++ app/src/main/java/com/example/items_to_cart/MainActivity.kt
@@ -2,34 +2,66 @@ package com.example.items_to_cart
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Adapter
+import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import org.w3c.dom.Text
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), AdapterProduct.OnAddToCartClickListener {
 
+    private lateinit var cartBadge: TextView
+    private var badgeCount: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val recyclerView: RecyclerView = findViewById(R.id.rv_products)
-        val layoutManager = GridLayoutManager(this,2)
+        val layoutManager = GridLayoutManager(this, 2)
         recyclerView.layoutManager = layoutManager
+
+        cartBadge = findViewById(R.id.cart_badge)
+        updateCartBadge()
         val products = createProduct()
         val adapter = AdapterProduct(products)
+
+        adapter.setOnAddToCartClickListener(this)
         recyclerView.adapter = adapter
+        recyclerView.adapter = adapter
+
+
     }
 
-    private fun createProduct():List<Product> {
+    private fun createProduct(): List<Product> {
         val productList = mutableListOf<Product>()
-        productList.add(Product("Mushroom","4.99$",R.drawable.pizza1))
-        productList.add(Product("Pepperoni"," 3.99$",R.drawable.pizza2))
-        productList.add(Product("Four Cheese"," 3.99$",R.drawable.pizza3))
-        productList.add(Product("White"," 3.99$",R.drawable.pizza4))
-        productList.add(Product("Vegetarian"," 3.99$",R.drawable.pizza5))
-        productList.add(Product("Margherita"," 3.99$",R.drawable.pizza6))
-        productList.add(Product("Vegetarian"," 3.99$",R.drawable.pizza5))
-        productList.add(Product("Margherita"," 3.99$",R.drawable.pizza6))
+        productList.add(Product("Mushroom", "4.99$", R.drawable.pizza1))
+        productList.add(Product("Pepperoni", " 3.99$", R.drawable.pizza2))
+        productList.add(Product("Four Cheese", " 3.99$", R.drawable.pizza3))
+        productList.add(Product("White", " 3.99$", R.drawable.pizza4))
+        productList.add(Product("Vegetarian", " 3.99$", R.drawable.pizza5))
+        productList.add(Product("Margherita", " 3.99$", R.drawable.pizza6))
+        productList.add(Product("Vegetarian", " 3.99$", R.drawable.pizza5))
+        productList.add(Product("Margherita", " 3.99$", R.drawable.pizza6))
         return productList
     }
+
+    override fun onAddToCartClick(position: Int) {
+        badgeCount++
+        updateCartBadge()
+        val clickedProduct = createProduct()[position]
+        Toast.makeText(this, "${clickedProduct.productName} added to cart", Toast.LENGTH_SHORT)
+            .show()
+    }
+
+    private fun updateCartBadge() {
+        if (badgeCount > 0) {
+            cartBadge.text = badgeCount.toString()
+            cartBadge.visibility = View.VISIBLE
+        } else {
+            cartBadge.visibility = View.GONE
+        }
+    }
+
 }
